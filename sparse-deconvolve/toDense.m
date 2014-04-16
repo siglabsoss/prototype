d9b60d8fb28dac6757cdeb0c0b2209b13dca9976
Ben Morse <ben@popwi.com>
2014-04-16 03:10:57 -0700

    * adding missing

---
+++ sparse-deconvolve/toDense.m
@@ -0,0 +1,17 @@
+function dense = toDense(sparse)
+
+[rows,A]=size(sparse);
+B = sparse(A)-sparse(1);
+dense = zeros(1,B);
+sig = -1;
+j = 1;
+
+for i = sparse(1):sparse(A)
+
+	if i == sparse(j)
+   		sig = sig * -1;
+   		j = j + 1;
+	end
+
+	dense(i-sparse(1)+1) = sig;
+end
