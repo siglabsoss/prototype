accc307ad7f06c9c0a00982bc9df16bf93113ec6
Ben Morse <bjmorse@gmail.com>
2014-09-18 21:01:59 -0700

    * better

--- matlab-ldpc/ldpcgen.m
+++ matlab-ldpc/ldpcgen.m
@@ -1,88 +1,92 @@
-function [ H ] = ldpcgen( n, k )
+
+function [ H, G ] = ldpcgen( n, k )
 %LDPCGEN Summary of this function goes here
 %   Detailed explanation goes here
 
-rndstate = [4112460541 4144164702 676943031 2084672537];
+rndstate = [4112460544 4144164702 676943031 2084672537];
 
 width = n;
 height = n-k;
-elements = width*height;
+
 
 H = zeros(height, width);
 
-onerows = 6;
-onecols = 6;
+onerows = 200;
 
-totalones = onerows*width + onecols*height;
-
-%totalones = 22;
-
-maxretry = 35;
-
-retry = 0;
 i = 1;
-while i <= totalones
-    [r,rndstate] = xor128(rndstate);
-    r = mod(r, elements);
+while i <= height
     
     
-    % calculate x and y of picked location
-    % this is tricky because it's 0 based
-    x = mod(r,width) + 1;
-    y = floor(r/(width)) + 1;
+    [rndstate,row] = onesrow(2, onerows, i, width, rndstate);
     
-    %     disp(sprintf('r: %d x: %d y: %d',r,x, y));
-    
-    if( retry > maxretry )
-        disp('Error: too many retries');
-        i = totalones+1;
-        continue;
+    j = 1;
+    [~,onesperrow] = size(row);
+    while j <= onesperrow
+        H(i,row(j)) = 1;
+        j = j + 1;
     end
-    
-    if(sum(H(:,x)) >= onecols)
-        retry = retry + 1;
-        disp(sprintf('bump by col (%d)', i));
-        continue;
-    end
-    
-            % if there are too many ones on this row, try again
-    if(sum(H(y,:)) >= onerows)
-        retry = retry + 1;
-        disp(sprintf('bump by row (%d)', i));
-        continue;
-    end
-    
 
-    
-
-    
-    
-    
-    retry = 0;
-    H(y,x) = 1;
     i = i + 1;
 end
 
+G = [];
+
 % checks
 % 
-% Hrr = g2rref(H);
-% Hl = Hrr(:,[1:height]);
-% if( sum(sum(Hl ~= eye(height))) )
-%     disp('Parity matrix H cannot be rrefd correctly');
-% end
-% 
-% 
-% G = ldpcpar2gen(H);
-% gok = mod(G*H',2);
-% if( sum(gok) ~= 0 )
-%     disp('Parity matrix H does not have valid generator G');
-% end
+Hrr = g2rref(H);
+Hl = Hrr(:,[1:height]);
+if( sum(sum(Hl ~= eye(height))) )
+    disp('Parity matrix H cannot be rrefd correctly');
+end
 
 
-
+G = ldpcpar2gen(H);
+gok = mod(G*H',2);
+if( sum(sum(gok)) ~= 0 )
+    disp('Parity matrix H does not have valid generator G');
+end
 
 
 
 
 end
+
+
+
+function [ state, outputrow ] = onesrow( onerowmin, onesrowmax, row, width, state )
+i = 0;
+% use the row index to affect the rng by burning variables
+while i < (mod(row,4)+1)
+    [burn,state] = xor128(state);
+    i = i + 1;
+end
+
+onespercol = mod(burn,onesrowmax-onerowmin) + onerowmin;
+
+%      disp('row')
+
+outputrow = [];
+i = 0;
+while i < onespercol
+    [col,state] = xor128(state);
+    col = mod(col, width)+1;
+    
+    outputrow(end+1)=col;
+    %     disp(col);s
+    i = i + 1;
+    
+end
+
+% disp(sprintf('ones for row(%d): %d', row, r));
+
+
+
+end
+
+
+
+
+
+
+
 
