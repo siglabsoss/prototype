38c4b78105dd7004bafee9a359b405f83982a9c8
Ben Morse <bjmorse@gmail.com>
2014-09-25 22:44:28 -0700

    * getting better at sparse

--- matlab-ldpc/ldpcgengen.m
+++ matlab-ldpc/ldpcgengen.m
@@ -13,7 +13,7 @@ width = n-k;
 height = k;
 
 
-A = zeros(height, width);
+A = spalloc(height, width, maxonecols*width);
 
 
 i = 1;
@@ -21,18 +21,52 @@ while i <= width
     
     
     [rndstate,col] = onesvector(minonecols, maxonecols, i, height, rndstate);
+
+    % the vector returned by onesvector is non sorted and may contain dupes
+    % if duplicated are present, all even number of duplicates should be
+    % removed
     
-    j = 1;
+    col = sort(col + 1);
+    
     [~,onespercol] = size(col);
+    
+    prev = col(1);
+
+    j = 2;
     while j <= onespercol
-        A((col(j)+1),i) = xor(  A((col(j)+1),i), 1);  % returned indices don't force a 1, they xor
+        if( col(j) == prev )
+            
+            col(j) = [];
+            col(j-1) = [];
+            onespercol = onespercol - 2;
+            j = j - 1; % going BACKWARDS here
+            
+            % special case when col looks like [3 3 3]
+            % this prevents underindex
+            if( j <= 1 )
+                break;
+            end
+            
+            prev = col(j-1); % grab prev with the decrimented j
+            continue;
+        end
+        prev = col(j); % grab j before inc
         j = j + 1;
     end
+    
+    % allocate vertical vector
+    column = spalloc(height, 1, onespercol);
+    
+    % using the sparce indices stored in col, set those entries to 1
+    column(col) = 1;
+    
+    % replace A with the column
+    A(:,i) = column;
 
     i = i + 1;
 end
 
-G = [eye(k) A];
+G = [speye(k) A];
 
 H = [];
 
@@ -43,20 +77,8 @@ H = [];
 % if( sum(sum(Hl ~= eye(height))) )
 %     disp('Parity matrix H cannot be rrefd correctly');
 % end
- 
-
-H = gen2par(G);
 
 valid = 1;
-
-gok = mod(G*H',2);
-if( sum(sum(gok)) ~= 0 )
-    if( ~silent )
-        disp('Parity matrix H does not have valid generator G');
-    end
-    valid = 0;
-    return
-end
 
 
 zerocol = sum(G,1);
@@ -69,16 +91,36 @@ if( min(zerocol) == 0 )
     return
 %     sum(G,1)
 end
+ 
+
+H = gen2par(G);
+
+
+
+% since gok is sparse
+gok = mod(G*H',2);
+
+% this nnz should be faster than sum(sum()) (untested)
+if( nnz(gok) ~= 0 )
+    if( ~silent )
+        disp('Parity matrix H does not have valid generator G');
+    end
+    valid = 0;
+    return
+end
+
+
+
 
 return
 
-disp('ldpccycle4check');
+% disp('ldpccycle4check');
 
-t = cputime();
+% t = cputime();
 
-cycle4s = ldpccycle4check(H);
+% cycle4s = ldpccycle4check(H);
 
-e = cputime() - t
+% e = cputime() - t
 
 if( cycle4s )
 %     if( ~silent )
@@ -104,13 +146,13 @@ onespercol = mod(burn,(onesrowmax-onerowmin+1)) + onerowmin;
 
 %      disp('row')
 
-outputrow = [];
+outputrow = zeros(1,onespercol);
 i = 0;
 while i < onespercol
     [col,state] = xor128(state);
     col = mod(col, width);
     
-    outputrow(end+1)=col;
+    outputrow(i+1)=col;
     %     disp(col);s
     i = i + 1;
     
