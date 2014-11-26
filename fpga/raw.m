301e60991cfa702d2a77d7b4bf10e96ff7232dfd
Ben Morse <bjmorse@gmail.com>
2014-11-25 22:04:28 -0800

    * capture 64bit from fpga

--- fpga/raw.m
+++ fpga/raw.m
@@ -2,7 +2,7 @@ function [ output ] = raw( samples )
 
 s = serial('COM3');
 set(s,'BaudRate',1250000);
-set(s,'InputBufferSize',100000000);
+set(s,'InputBufferSize',10000000);
 fopen(s);
 
 c = onCleanup(@()fclose(s));
@@ -16,12 +16,13 @@ samples = 50000;
 output = zeros(1,samples);
 
 charChunk = 25000;
+bytesPerSample = 8;
 
 for i=1:(samples/charChunk)
-    A = fread(s,4*charChunk);
+    A = fread(s,bytesPerSample*charChunk);
     % only works for uint32
     %number = A(1) + A(2)*256 + A(3)*65536 + A(4)*16777216; 
-    numbers = typecast(uint8(A),'int32');
+    numbers = typecast(uint8(A),'int64');
 %     disp(number);
 
     output(((i-1)*charChunk)+1:i*charChunk) = numbers;
@@ -33,7 +34,7 @@ for i=1:(samples/charChunk)
     
 
 %     if( mod(number,1000) == 0 )
-        %disp(sprintf('%d %s', numbers(1), mat2str(A(1:4))));
+%         disp(sprintf('%d %s', numbers(1), mat2str(A(1:bytesPerSample))));
 %     end
 
 end
