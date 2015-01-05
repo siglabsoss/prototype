738e226931be5428dda174b19206011cff94fd31
Ben Morse <bjmorse@gmail.com>
2015-01-05 15:36:11 -0800

    * wrestling to get 3 output ports

--- simulink/mycpm.m
+++ simulink/mycpm.m
@@ -21,9 +21,16 @@ outSampleTime = inSampleTime / samplesPerSymbol;
 
 % block.OutputPort(1).SampleTime = [0.05 0.05];
 block.OutputPort(1).SampleTime = [outSampleTime 0];
+block.OutputPort(2).SampleTime = [outSampleTime 0];
+block.OutputPort(3).SampleTime = [outSampleTime 0];
 
-disp(1);
 
+
+%end
+
+function SetInputPortSamplingMode(block, port, mode)
+% When a Level-2 MATLAB S-function with multiple output ports has dynamic sampling mode setting for any of its ports, it is necessary to register a 'SetInputPortSamplingMode' method
+block.InputPort(1).SamplingMode = 0; % 0 = sample 1 = frame
 %end
 
 function SetOutputPortSampleTime(block, portNumber, time)
@@ -45,7 +52,7 @@ samplesPerSymbol = eval(get_param(gcb,'SampsPerSym'));
 % cc = block.DialogPrm(3).Data;
 
 block.NumInputPorts = 1;
-block.NumOutputPorts = 1;
+block.NumOutputPorts = 3;
 
 block.InputPort(1).Complexity = 'Real';
 block.InputPort(1).DataTypeID = 0; % 8 for boolean, 0 for double
@@ -55,7 +62,15 @@ block.InputPort(1).SampleTime = [-1 0];
 
 block.OutputPort(1).DatatypeID  = 0; % double
 block.OutputPort(1).Complexity  = 'Complex';
+block.OutputPort(1).SamplingMode = 'Sample';
 
+block.OutputPort(2).DatatypeID  = 0; % double
+block.OutputPort(2).Complexity  = 'Complex';
+block.OutputPort(2).SamplingMode = 'Sample';
+
+block.OutputPort(3).DatatypeID  = 0; % double
+block.OutputPort(3).Complexity  = 'Complex';
+block.OutputPort(3).SamplingMode = 'Sample';
 
 
 
@@ -64,6 +79,7 @@ block.RegBlockMethod('Start', @Start);
 block.RegBlockMethod('Outputs', @Outputs);
 block.RegBlockMethod('SetInputPortSampleTime', @SetInputPortSampleTime);
 block.RegBlockMethod('SetOutputPortSampleTime', @SetOutputPortSampleTime);
+block.RegBlockMethod('SetInputPortSamplingMode', @SetInputPortSamplingMode);
 %end
 
 function InitVars()
@@ -151,6 +167,8 @@ beta = 1 - alpha;
 
 % write to block
 block.OutputPort(1).Data = complex(rout,iout);
+block.OutputPort(2).Data = complex(rout,iout);
+block.OutputPort(3).Data = complex(rout,iout);
 
 
 totalSamples = totalSamples + 1;
