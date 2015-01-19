d13b6e3646247a96edd3da59ba9d9cbb55295f74
Edwin <inceptionev@gmail.com>
2015-01-19 15:27:59 -0800

    trying to fix time delay

--- simulink/aligntones.m
+++ simulink/aligntones.m
@@ -10,10 +10,10 @@ fa2 = findtones(data1);
 subplot 411
 plot(timestamp,real(data))
 subplot 412
-plot(timestamp,sin(2*pi*timestamp*(fa1(1,1)+fa1(2,1))+fa1(1,2)-fa1(2,2)))
+plot(timestamp,sin(2*pi*timestamp*(fa1(1,1)+fa1(2,1))+fa1(1,2)+fa1(2,2)))
 subplot 413
 
 
 plot(timestamp,real(data1))
 subplot 414
-plot(timestamp,sin(2*pi*timestamp*(fa2(1,1)+fa2(2,1))+fa2(1,2)-fa2(2,2)))
+plot(timestamp,sin(2*pi*timestamp*(fa2(1,1)+fa2(2,1))+fa2(1,2)+fa2(2,2)))
