29e21efcb514599237f528d7c812d5dc24477c8c
Ben Morse <bjmorse@gmail.com>
2015-02-02 22:15:51 -0800

    * adding cross correlation example with instructions in readme

---
+++ simulink/Readme.txt
@@ -0,0 +1,4 @@
+Coherent summing using cross correlation:
+ - Load xcorrdata.mat
+ - Run benfindpacket
+ - Open and run demod_my_cpm
--- simulink/findpacket.m
+++ simulink/findpacket.m
@@ -1,30 +1,19 @@
-function [ output_args ] = findpacket( )
+function [ packet, startSample ] = findpacket(clock_comb, data )
 
-load('xcorrdata.mat');
+% load('xcorrdata.mat');
 % data
+% data1
 % clock_comb
 
 [combLength,~] = size(clock_comb);
 
-% this order matters
-xcr = xcorr(clock_comb,data);
+% this order matters, data first (longer) for a positive value of lag
+[xcr, lag] = xcorr(data,clock_comb);
 
 % sample at maximum
 [maxVal, maxSample] = max(abs(xcr));
 
-% xcorr returns the (last?) sample that matches, so subtract the comb
-% lenght for first
-startSample = maxSample - combLength;
+% lag between comb and sample at max
+startSample = lag(maxSample);
 
 packet = data(startSample:startSample+combLength);
-
-% cheating
-packet = data(3034:3034+combLength-1);
-
-
-
-foo = bar;
-
-
-end
-
