19c30e090c4b68f64ae3b649b17a604609ff17df
Ben Morse <bjmorse@gmail.com>
2015-02-06 17:18:13 -0800

    * unrollangle works great

---
+++ simulink/unrollangle.m
@@ -0,0 +1,32 @@
+function [ output ] = unrollangle( input )
+%UNROLLANGLE pass the output of angle() into this to remove discontinuous
+%samples
+
+thresh = pi/2;
+
+adjust = 0;
+
+[sz,~] = size(input);
+
+output = zeros(sz,1);
+
+% first sample b/c loop below starts at 2
+output(1) = input(1);
+
+for index = 2:sz
+    samp = input(index);
+    prev = input(index-1);
+    
+    if(abs(samp-prev) > thresh)
+        direction = 1;
+        if( samp > prev )
+            direction = -1;
+        end
+        adjust = adjust + 2*pi*direction;
+    end
+    
+    output(index) = input(index) + adjust;
+end
+
+end
+
