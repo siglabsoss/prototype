96f85e780a0829114d5b249f35955066dd2e50c6
Ben Morse <bjmorse@gmail.com>
2015-03-20 15:34:39 -0700

    * limiting final bw

--- simulink/opti_comb2.m
+++ simulink/opti_comb2.m
@@ -7,7 +7,9 @@ function [ dout ] = opti_comb2(  )
 
 samples = 25000;
 
-sts = [-125:0.01:125];
+stsdiv = 100;
+
+sts = [samples/stsdiv/-2:1/stsdiv:samples/stsdiv/2];
 
 a1 = 180;
 
@@ -18,9 +20,30 @@ ss2 = cos(ss) + 1i*sin(ss);
 
 oc2 = ss2';
 
+% figure;
+% plot(real(oc2));
+% figure;
+% splot(oc2);
 
-oc2chop = bandwidth_chop(oc2,25000,-110,110);
-ocrand = sig_normalize(oc2chop(1:11500)) + (rand_walk(11500)')/1;
+
+firstBwChop = 110;
+
+% bandwith limit
+oc2bwlim = bandwidth_chop(oc2,25000,-1*firstBwChop,firstBwChop);
+
+oc2chop = oc2bwlim(1:11500);
+
+% higher means more rand walk
+randWalkMixLevel = 0.9;
+
+
+ocrand = sig_normalize(oc2chop) + (rand_walk(11500)') * randWalkMixLevel;
+
+
+secondBwChop = 700;
+
+ocrandlim = bandwidth_chop(ocrand,25000,-1*secondBwChop,secondBwChop);
+
 
 dout = ocrand;
 
