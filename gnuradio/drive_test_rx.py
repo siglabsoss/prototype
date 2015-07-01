d371fa99865f7d28940741ac1620ba4bd5639b9d
Ben Morse <bjmorse@gmail.com>
2015-06-30 20:28:53 -0700

    * drive test python

--- gnuradio/drive_test_rx.py
+++ gnuradio/drive_test_rx.py
@@ -49,25 +49,22 @@ class top_block(grc_wxgui.top_block_gui):
         self.uhd_usrp_source_0_0.set_clock_source("external", 0)
         self.uhd_usrp_source_0_0.set_samp_rate(samp_rate)
         self.uhd_usrp_source_0_0.set_center_freq(samp_freq, 0)
-        self.uhd_usrp_source_0_0.set_gain(all_rx_gain, 0)
-        self.uhd_usrp_source_0_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_source_0_0.set_gain(0, 0)
+        self.uhd_usrp_source_0_0.set_antenna("RX2", 0)
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_file_sink_1 = blocks.file_sink(gr.sizeof_gr_complex*1, "drive_test.raw", False)
         self.blocks_file_sink_1.set_unbuffered(False)
-        self.band_pass_filter_0_1 = filter.fir_filter_ccc(1, firdes.complex_band_pass(
-        	1, samp_rate, 18E3, 22E3, 1e3, firdes.WIN_HAMMING, 6.76))
         self.band_pass_filter_0 = filter.fir_filter_ccc(1, firdes.complex_band_pass(
-        	1, samp_rate, 18E3, 22E3, 1e3, firdes.WIN_HAMMING, 6.76))
+        	1, samp_rate, 15E3, 25E3, 1e3, firdes.WIN_HAMMING, 6.76))
         self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, -20000, 1, 0)
 
         ##################################################
         # Connections
         ##################################################
         self.connect((self.uhd_usrp_source_0_0, 0), (self.band_pass_filter_0, 0))
-        self.connect((self.band_pass_filter_0, 0), (self.band_pass_filter_0_1, 0))
         self.connect((self.analog_sig_source_x_1, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.band_pass_filter_0_1, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_file_sink_1, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.blocks_multiply_xx_0, 0))
 
 
 # QT sink close method reimplementation
@@ -83,10 +80,9 @@ class top_block(grc_wxgui.top_block_gui):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.uhd_usrp_source_0_0.set_samp_rate(self.samp_rate)
-        self.band_pass_filter_0.set_taps(firdes.complex_band_pass(1, self.samp_rate, 18E3, 22E3, 1e3, firdes.WIN_HAMMING, 6.76))
         self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
-        self.band_pass_filter_0_1.set_taps(firdes.complex_band_pass(1, self.samp_rate, 18E3, 22E3, 1e3, firdes.WIN_HAMMING, 6.76))
+        self.uhd_usrp_source_0_0.set_samp_rate(self.samp_rate)
+        self.band_pass_filter_0.set_taps(firdes.complex_band_pass(1, self.samp_rate, 15E3, 25E3, 1e3, firdes.WIN_HAMMING, 6.76))
 
     def get_samp_freq(self):
         return self.samp_freq
