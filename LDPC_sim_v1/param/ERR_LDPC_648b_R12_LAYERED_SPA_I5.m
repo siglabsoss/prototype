dea263f7ea650fe544a975e71050f2bff61fd5f6
Kraken <bjmorse@gmail.com>
2016-02-03 16:49:23 -0800

    fix exception under octave

--- LDPC_sim_v1/param/ERR_LDPC_648b_R12_LAYERED_SPA_I5.m
+++ LDPC_sim_v1/param/ERR_LDPC_648b_R12_LAYERED_SPA_I5.m
@@ -3,7 +3,7 @@ function ERR_LDPC_648b_R12_LAYERED_SPA_I5(RunID)
   % == LDPC SETTINGS ====================================
 
   TxRx.Sim.name = 'ERR_LDPC_648b_R12_LAYERED_SPA_I5';
-  TxRx.Sim.nr_of_channels = 1000; % 1k for good results, 10k for accurate results
+  TxRx.Sim.nr_of_channels = 5; % 1k for good results, 10k for accurate results
   TxRx.Sim.SNR_dB_list = [0:1:8];
   TxRx.Decoder.LDPC.Scheduling = 'Layered'; % 'Layered' and 'Flooding'
   TxRx.Decoder.LDPC.Type = 'SPA'; % 'MPA' and 'SPA' (optimal)
@@ -11,6 +11,9 @@ function ERR_LDPC_648b_R12_LAYERED_SPA_I5(RunID)
   load('codes/LDPC_11nD2_648b_R12.mat'); % load code
   
   % == EXECUTE SIMULATION ===============================  
+  
+  LDPC.H = double(LDPC.H.x);
+  LDPC.G = double(LDPC.G.x);
   
   sim_LDPC(RunID,TxRx,LDPC) 
   
